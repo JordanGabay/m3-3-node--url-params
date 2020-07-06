4237b755004ef93162498fe23246e684189ffa92
Jordan Gabay <gabay.jordan@outlook.com>
2020-07-06 13:24:07 -0400

    Exercise 1.1 and 1.2 Complete

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 'use strict';
 
+const express = require('express');
 const morgan = require('morgan');
+const app = express();
 
 const { top50 } = require('./data/top50');
 
 const PORT = process.env.PORT || 8000;
 
-const app = express();
-
 app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
+//set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // endpoints here
+app.get('/top50', (req, res) => {
+    res.status(404);
+    res.render('pages/top50', {
+        title: 'Top 50 Songs Streamed on Spotify',
+        myName: 'Jordan',
+        top50,
+        path: req.originalUrl
+    });
+});
+
+
 
 // handle 404s
-app.git('*', (req, res) => {
+app.get('*', (req, res) => {
     res.status(404);
     res.render('pages/fourOhFour', {
         title: 'I got nothing',
@@ -24,4 +36,4 @@ app.git('*', (req, res) => {
     });
 });
 
-get.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
