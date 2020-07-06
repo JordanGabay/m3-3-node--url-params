8df4c5602cb37d392eb608d571cadeb2a793439c
Jordan Gabay <gabay.jordan@outlook.com>
2020-07-06 15:31:07 -0400

    1.6 Complete

--- server.js
+++ server.js
@@ -25,6 +25,30 @@ app.get('/top50', (req, res) => {
     });
 });
 
+// app.get('/top50popular-artist', (req, res) => {
+//     res.status(404);
+//     res.render('pages/top50', {
+//         title: 'Most Popular Artist',
+//         myName: 'Jordan',
+//         top50,
+//         path: req.originalUrl
+//     });
+// });
+
+app.get('/top50/:song', (req, res) => {
+    res.status(404);
+    const {song} = req.params
+    console.log(top50[song - 1].title)
+    console.log(top50[song - 1].artist)
+    res.render('pages/singlesong', {
+        title: 'Song #',
+        myName: 'Jordan',
+        song: top50[song - 1].title,
+        artist: top50[song - 1].artist,
+        streams: top50[song - 1].streams,
+        path: req.originalUrl
+    });
+});
 
 
 // handle 404s
